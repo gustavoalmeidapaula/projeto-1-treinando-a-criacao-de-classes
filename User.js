ff023b007c5bbd31705ea0dce7ffb99b715275d6
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-08-07 08:44:07 -0300

    Update User.js

--- User.js
+++ User.js
@@ -1,17 +1,17 @@
-class User {
+class User {           
     constructor(fullname, email, password) {
-      this.fullname = fullname;
-      this.email = email;
-      this.password = password;
+      this.fullname = fullname
+      this.email = email
+      this.password = password
     }
     login(email, password) {
       if (this.email === email && this.password === password) {
-        console.log("Login bem-sucedido!");
+        console.log('Login bem-sucedido!')
       } else {
-        console.log("Login falhou. Verifique suas credenciais.");
+        console.log('Login falhou. Verifique suas credenciais.')
       }
     }
   }
   
-  const user1 = new User("Everson Sousa", "evs@example.com", "123456");
-  user1.login("evs@example.com", "12345");
+  const user = new User('Everson Sousa', 'evs@example.com', '123456')
+  user.login('evs@example.com', '123456')
